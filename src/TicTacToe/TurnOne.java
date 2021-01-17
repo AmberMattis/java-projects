c1ba1d58996c340a778f69cae0cb480b91b50934
Amber Mattis <amber.mattis2@gmail.com>
2021-01-16 19:06:10 -0600

    created board displays for turn one

---
+++ src/TicTacToe/TurnOne.java
@@ -0,0 +1,150 @@
+package TicTacToe;
+
+public class TurnOne {
+    public static void rowOneColOneX() {
+        System.out.println("      |      |      ");
+        System.out.println("   X  |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+    }
+
+    public static void rowOneColTwoX() {
+        System.out.println("      |      |      ");
+        System.out.println("      |   X  |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+    }
+
+
+    public static void rowOneColThreeX() {
+        System.out.println("      |      |      ");
+        System.out.println("      |      |   X  ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+    }
+
+
+
+    public static void rowTwoColOneX() {
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("   X  |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+    }
+
+
+
+    public static void rowTwoColTwoX() {
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |   X  |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+    }
+
+
+
+    public static void rowTwoColThreeX() {
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |   X  ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+    }
+
+
+
+    public static void rowThreeColOneX() {
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("   X  |      |      ");
+        System.out.println("      |      |      ");
+    }
+
+
+
+    public static void rowThreeColTwoX() {
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |   X  |      ");
+        System.out.println("      |      |      ");
+    }
+
+
+
+    public static void rowThreeColThreeX() {
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |   X  ");
+        System.out.println("      |      |      ");
+    }
+
+
+    public static void main(String[] args) {
+
+        rowOneColTwoX();
+    }
+
+
+}
--- src/TicTacToe/main.java
+++ src/TicTacToe/main.java
@@ -3,6 +3,8 @@ package TicTacToe;
 public class main {
     public static void main(String[] args){
         Menu menu = new Menu();
-        Menu.gameMenu();
+        TurnOne board = new TurnOne();
+
+
     }
 }
