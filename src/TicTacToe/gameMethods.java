0bb03b00cf9fc0bd26c41e5584329d7af0f8935c
Amber Mattis <amber.mattis2@gmail.com>
2021-01-16 18:17:39 -0600

    created game menu display

---
+++ src/TicTacToe/gameMethods.java
@@ -0,0 +1,23 @@
+package TicTacToe;
+
+public class gameMethods {
+//    Create a tic tac toe game in javas
+//
+//    1. Display tic tac toe title
+//    2. underneath title display game menu
+//    3. menu should give the users the options to start a new game or exit application entirely
+//    4. Display player one and player 2 text
+//    5. During a game session, the player one/player two text with alternate the color red to signify player's turn
+//    6. Player should have the option to exit a game session at anytime.
+
+
+    public static void gameMenu(){
+        System.out.println("*********************");
+        System.out.println("*     GAME MENU     *");
+        System.out.println("*********************");
+        System.out.println("* 1. Start new game *");
+        System.out.println("* 2. Exit           *");
+        System.out.println("*********************");
+
+    }
+}
