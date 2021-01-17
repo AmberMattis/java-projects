a0f1c5d16989023afd0bc3992a83290b08eaa185
Amber Mattis <amber.mattis2@gmail.com>
2021-01-16 22:11:35 -0600

    added method to display a second turn result possibility

--- src/TicTacToe/TurnTwoBoard.java
+++ src/TicTacToe/TurnTwoBoard.java
@@ -1,4 +1,17 @@
 package TicTacToe;
 
 public class TurnTwoBoard {
+    public static void rowOneColOneO() {
+        System.out.println("      |      |      ");
+        System.out.println("   X  |  O   |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("--------------------");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+        System.out.println("      |      |      ");
+    }
 }
