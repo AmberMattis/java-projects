f01d9403d8997c8874469961bec94526b527417b
Amber Mattis <amber.mattis2@gmail.com>
2021-01-16 20:47:28 -0600

    created turnTwoBoard class

--- src/TicTacToe/TurnOneMethod.java
+++ src/TicTacToe/TurnOneMethod.java
@@ -2,36 +2,32 @@ package TicTacToe;
 
 public class TurnOneMethod {
 
+        public static void turnOnePlay(){
+        System.out.println("Enter something");
 
+         Input user = new Input();
+         Integer userNum = user.userInput;
 
-    public static void turnOnePlay(){
-    System.out.println("Enter something");
-
-    Input user = new Input();
-    Integer userNum = user.userInput;
-
-        if (userNum.equals(1)){
-        TurnOneBoard.rowOneColOneX();
-    }   if (userNum.equals(2)){
+            if (userNum.equals(1)){
+            TurnOneBoard.rowOneColOneX();
+        }   if (userNum.equals(2)){
             TurnOneBoard.rowOneColTwoX();
-    }   if (userNum.equals((3))){
+        }   if (userNum.equals((3))){
             TurnOneBoard.rowOneColThreeX();
-    }   if (userNum.equals(4)){
+        }   if (userNum.equals(4)){
             TurnOneBoard.rowTwoColOneX();
-    }   if (userNum.equals(5)){
+        }   if (userNum.equals(5)){
             TurnOneBoard.rowTwoColTwoX();
-    }   if (userNum.equals(6)){
+        }   if (userNum.equals(6)){
             TurnOneBoard.rowTwoColThreeX();
-    }  if (userNum.equals(7)){
+        }  if (userNum.equals(7)){
             TurnOneBoard.rowThreeColOneX();
-    }  if (userNum.equals(8)){
+        }  if (userNum.equals(8)){
             TurnOneBoard.rowThreeColTwoX();
-    }  if (userNum.equals(9)){
+        }  if (userNum.equals(9)){
             TurnOneBoard.rowThreeColThreeX();
-    } else if (userNum > 9 || userNum < 1) {
+        } else if (userNum > 9 || userNum < 1) {
         System.out.println("goodbye");
+        }
     }
-}
-
-
 }
--- src/TicTacToe/main.java
+++ src/TicTacToe/main.java
@@ -2,8 +2,6 @@ package TicTacToe;
 
 public class main extends Menu {
     public static void main(String[] args) {
-//        Menu menu = new Menu();
-
         menuSelection();
 
 
